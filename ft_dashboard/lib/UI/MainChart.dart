df234a4d70aa5e27e1758373d1236e9b6e3e1875
Guilherme Baia <guilhermebaiav@gmail.com>
2024-05-20 09:45:56 -0300

    add bottom title function to main chart

--- ft_dashboard/lib/UI/MainChart.dart
+++ ft_dashboard/lib/UI/MainChart.dart
@@ -17,7 +17,7 @@ class MainChart extends StatelessWidget {
             );
           },
         ),
-        titlesData: const FlTitlesData(
+        titlesData: FlTitlesData(
           leftTitles: AxisTitles(
             sideTitles: SideTitles(
               showTitles: true,
@@ -30,6 +30,7 @@ class MainChart extends StatelessWidget {
               showTitles: true,
               interval: 1,
               reservedSize: 30,
+              getTitlesWidget: getBottomTitle,
             ),
           ),
           topTitles: AxisTitles(
@@ -52,12 +53,11 @@ class MainChart extends StatelessWidget {
             isCurved: true,
             spots: [
               const FlSpot(0, 0),
-              const FlSpot(3, 1.5),
-              const FlSpot(5, 2.4),
-              const FlSpot(7, 3.4),
-              const FlSpot(10, 4),
-              const FlSpot(13, 5.2),
-              const FlSpot(16, 6),
+              const FlSpot(1, 1.5),
+              const FlSpot(2, 2.4),
+              const FlSpot(3, 3.4),
+              const FlSpot(4, 4),
+              const FlSpot(5, 4),
             ],
           ),
           LineChartBarData(
@@ -69,31 +69,49 @@ class MainChart extends StatelessWidget {
               const FlSpot(2, 2.4),
               const FlSpot(3, 3.4),
               const FlSpot(4, 4),
-              const FlSpot(5, 5.2),
-              const FlSpot(6, 6),
-              const FlSpot(7, 4),
-              const FlSpot(8, 1.5),
-              const FlSpot(9, 2.4),
-              const FlSpot(10, 3.4),
-              const FlSpot(11, 4),
-              const FlSpot(12, 5.2),
-              const FlSpot(16, 6),
+              const FlSpot(5, 5),
             ],
           ),
           LineChartBarData(
             isCurved: true,
             spots: [
               const FlSpot(0, 0),
-              const FlSpot(2, 1.5),
-              const FlSpot(4, 1.4),
-              const FlSpot(6, 3.4),
-              const FlSpot(8, 2),
-              const FlSpot(10, 2.2),
-              const FlSpot(12, 1.8),
+              const FlSpot(1, 1.5),
+              const FlSpot(2, 1.4),
+              const FlSpot(3, 3.4),
+              const FlSpot(4, 2),
+              const FlSpot(5, 2.2),
             ],
           ),
         ],
       ),
     );
   }
+}
+
+Widget getBottomTitle(value, meta) {
+  String text = "";
+
+  switch (value.toInt()) {
+    case 0:
+      text = "2022.1";
+    case 1:
+      text = "2022.2";
+    case 2:
+      text = "2023.1";
+    case 3:
+      text = "2023.2";
+    case 4:
+      text = "2024.1";
+    case 5:
+      text = "2024.2";
+  }
+  return Text(
+    text,
+    style: TextStyle(
+      color: Colors.grey[600],
+      fontSize: 12,
+    ),
+  );
+  return Text("a");
 }
