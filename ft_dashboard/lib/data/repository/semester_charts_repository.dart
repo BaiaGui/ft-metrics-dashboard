06ecbdb7f94dae58460f27fd4ca3617f3e588ce7
Guilherme Baia <guilhermebaiav@gmail.com>
2024-05-31 12:18:27 -0300

    add initial function to semester repository

--- ft_dashboard/lib/data/repository/semester_charts_repository.dart
+++ ft_dashboard/lib/data/repository/semester_charts_repository.dart
@@ -1,1 +1,28 @@
-class SemesterChartsRepository {}
+import 'package:ft_dashboard/data/provider/course_provider.dart';
+
+class SemesterChartsRepository {
+  final CourseProvider courseProvider = CourseProvider();
+
+  //retornar lista de models para cada curso que tem:
+  // - lista com [6] valores,
+  // - nome do curso,
+  getSemesterChartsData({required int year, required int semester}) async {
+    final allCourses = await courseProvider.getAllCoursesData();
+    List chartsData = [];
+    for (var course in allCourses) {
+      var courseValues = getCourseData(course.id);
+      chartsData.add([course.name, courseValues]);
+    }
+    return chartsData;
+  }
+
+  //Função que dado um curso Id, ano e semestre
+  //retorna proporção de respostas do curso e período
+  //- pegar todos as turmas do período
+  //- Filtrar turma pelo curso
+  //- pegar todos os forms de cada turma
+  //- calcular proporção de resposta de todos os forms
+  //- Armazenar em uma list
+
+  getCourseData(id) {}
+}
