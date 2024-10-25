76b23636356fe33ea11437f87c4ecad57b573147
Guilherme Baia <guilhermebaiav@gmail.com>
2024-10-24 21:19:27 -0300

    add index calculation function

--- ft_dashboard/back-end/controllers/main_chart_controller.js
+++ ft_dashboard/back-end/controllers/main_chart_controller.js
@@ -14,18 +14,24 @@ class MainChartController {
   }
 
   async getIndex(req, res) {
-    //category is defined here
     let year = 2022;
     let semester = 2;
-    let filteredForms = await getFormsByTime(year, semester);
-    //let answerProportion = getAnswerProportion(filteredForms);
-    //let index = calculateIndexByCategory(filteredForms, 1);
+    let answerProportion = await getAnswerProportionByTime(year, semester);
+    let indexInfra = calculateIndexByCategory(answerProportion[0]);
+    let indexStudent = calculateIndexByCategory(answerProportion[1]);
+    let indexTeacher = calculateIndexByCategory(answerProportion[2]);
 
-    res.send(filteredForms);
+    res.send({
+      year: year,
+      semester: semester,
+      indexInfra,
+      indexStudent,
+      indexTeacher,
+    });
   }
 }
 
-async function getFormsByTime(year, semester) {
+async function getAnswerProportionByTime(year, semester) {
   try {
     const cohorts = db.collection("cohorts");
     const formGroup = await cohorts
@@ -55,7 +61,6 @@ async function getFormsByTime(year, semester) {
             form: 1,
           },
         },
-        //TEPORARY STAGES: LIMITING NUMBER OF DOCUMENTS TO VERIFY COUNT ALGORITHM
         {
           $unwind: {
             path: "$form",
@@ -71,6 +76,12 @@ async function getFormsByTime(year, semester) {
             path: "$questoes",
           },
         },
+        /**
+         * IMPORTANTE!
+         * Para calcular o TIPO de resposta (concordo parcialmente, discordo totalmente, ...),
+         * a função considera o NÚMERO da resposta. Ou seja, 1 = DT, 2 = DP, ...
+         *
+         */
         {
           $bucket: {
             groupBy: {
@@ -88,7 +99,7 @@ async function getFormsByTime(year, semester) {
         {
           $project: {
             _id: "$_id",
-            contagem: {
+            typeCount: {
               $reduce: {
                 input: "$category_answers",
                 initialValue: {
@@ -180,181 +191,13 @@ async function getFormsByTime(year, semester) {
   }
 }
 
-/**
- * IMPORTANTE!
- * Para calcular o TIPO de resposta (concordo parcialmente, discordo totalmente, ...),
- * a função considera o NÚMERO da resposta. Ou seja, 1 = DT, 2 = DP, ...
- *
- */
-function getAnswerProportion(forms) {
-  let answerProportion = [0, 0, 0, 0, 0, 0];
-  for (let i = 0; i < forms.length; i++) {
-    for (let j = 0; j < forms[i].questoes.length - 2; j++) {
-      let answer = forms[i].questoes[j].resposta;
-      answerProportion[answer]++;
-    }
-  }
-  return answerProportion;
+function calculateIndexByCategory(answerProportion) {
+  let { count_0, count_1, count_2, count_3, count_4, count_5 } = answerProportion.typeCount;
+  let sum = 5 * count_5 + 4 * count_4 + 3 * count_3 + 2 * count_2 + 1 * count_1;
+  let validAnswers = count_1 + count_2 + count_3 + count_4 + count_5;
+  let index = (sum / validAnswers - 1) / 4;
+
+  return index;
 }
 
-function calculateIndexByCategory(formData, category) {}
-
 module.exports = new MainChartController();
-
-const query = [
-  //stage 2: get related form data
-  {
-    $lookup: {
-      from: "forms",
-      localField: "codTurma",
-      foreignField: "codTurma",
-      as: "form",
-    },
-  },
-  //stage 3: filter properties
-  {
-    $project: {
-      codTurma: 1,
-      ano: 1,
-      semestre: 1,
-      form: 1,
-    },
-  },
-  //TEPORARY STAGES: LIMITING NUMBER OF DOCUMENTS TO VERIFY COUNT ALGORITHM
-  {
-    $unwind: {
-      path: "$form",
-    },
-  },
-  {
-    $replaceRoot: {
-      newRoot: "$form",
-    },
-  },
-  {
-    $unwind:
-      /**
-       * path: Path to the array field.
-       * includeArrayIndex: Optional name for index.
-       * preserveNullAndEmptyArrays: Optional
-       *   toggle to unwind null and empty values.
-       */
-      {
-        path: "$questoes",
-      },
-  },
-  {
-    $bucket:
-      /**
-       * groupBy: The expression to group by.
-       * boundaries: An array of the lower boundaries for each bucket.
-       * default: The bucket name for documents that do not fall within the specified boundaries
-       * output: {
-       *   outputN: Optional. The output object may contain a single or numerous field names used to accumulate values per bucket.
-       * }
-       */
-      {
-        groupBy: {
-          $toInt: "$questoes.numero_pergunta",
-        },
-        boundaries: [1, 8, 15, 25],
-        default: "Others",
-        output: {
-          category_answers: {
-            $push: "$questoes.resposta",
-          },
-        },
-      },
-  },
-  {
-    $project:
-      /**
-       * _id: The id of the group.
-       * fieldN: The first field name.
-       */
-      {
-        _id: "$_id",
-        contagem: {
-          $reduce: {
-            input: "$category_answers",
-            initialValue: {
-              count_0: 0,
-              count_1: 0,
-              count_2: 0,
-              count_3: 0,
-              count_4: 0,
-              count_5: 0,
-            },
-            in: {
-              count_0: {
-                $cond: [
-                  {
-                    $eq: ["$$this", "0"],
-                  },
-                  {
-                    $add: ["$$value.count_0", 1],
-                  },
-                  "$$value.count_0",
-                ],
-              },
-              count_1: {
-                $cond: [
-                  {
-                    $eq: ["$$this", "1"],
-                  },
-                  {
-                    $add: ["$$value.count_1", 1],
-                  },
-                  "$$value.count_1",
-                ],
-              },
-              count_2: {
-                $cond: [
-                  {
-                    $eq: ["$$this", "2"],
-                  },
-                  {
-                    $add: ["$$value.count_2", 1],
-                  },
-                  "$$value.count_2",
-                ],
-              },
-              count_3: {
-                $cond: [
-                  {
-                    $eq: ["$$this", "3"],
-                  },
-                  {
-                    $add: ["$$value.count_3", 1],
-                  },
-                  "$$value.count_3",
-                ],
-              },
-              count_4: {
-                $cond: [
-                  {
-                    $eq: ["$$this", "4"],
-                  },
-                  {
-                    $add: ["$$value.count_4", 1],
-                  },
-                  "$$value.count_4",
-                ],
-              },
-              count_5: {
-                $cond: [
-                  {
-                    $eq: ["$$this", "5"],
-                  },
-                  {
-                    $add: ["$$value.count_5", 1],
-                  },
-                  "$$value.count_5",
-                ],
-              },
-            },
-          },
-        },
-      },
-  },
-];
